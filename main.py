7432cc259db319e8008a450cfbaa2833cf1de717
stantheguru <sirstanleyzurki@gmail.com>
2023-05-09 19:03:59 +0300

    updates post

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import psycopg2
 
 # establish a connection
@@ -41,3 +41,11 @@ async def hello():
         cur.close()
         conn.close()
     return {"message":results}
+
+@app.post("/post")
+async def test(request: Request):
+    data = await request.json()
+
+    # Access the value based on the key
+    value = data.get("name")
+    return value
